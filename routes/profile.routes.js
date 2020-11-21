d47b7708f05f06ae18102e475940c7f04252d719
Marisa Vitale <marisa.vitale2306@gmail.com>
2020-11-21 19:16:52 +0100

    changes

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -3,11 +3,11 @@ const router = express.Router()
 
 const Farm = require('../models/farm.model')
 
-const ensureAuthenticated = (req, res, next) => req.isAuthenticated() ? next() : res.render('auth/login-form', { errorMsg: 'Desautorizado, inicia sesión' })
-const checkRole = admittedRoles => (req, res, next) => admittedRoles.includes(req.user.role) ? next() : res.render('auth/login-form', { errorMsg: 'Desautorizado, no tienes permisos' })
+const ensureAuthenticated = (req, res, next) => req.isAuthenticated() ? next() : res.render('auth/login-form', { errorMsg: 'You are not authorized, please log in' })
+const checkRole = admittedRoles => (req, res, next) => admittedRoles.includes(req.user.role) ? next() : res.render('auth/login-form', { errorMsg: 'Not authorized' })
 
 
-router.get('/', ensureAuthenticated, checkRole(['FARMER', 'BUYER', 'ADMIN']), (req, res) => res.render('profiles/profile'), { user: req.user, isFarmer: req.user.role.includes('FARMER') })
+router.get('/', ensureAuthenticated, checkRole(['FARMER', 'BUYER', 'ADMIN']), (req, res) => res.render('profiles/profile', { user: req.user, isFarmer: req.user.role.includes('FARMER') }))
 router.get('/create-farm', (req, res) => res.render('profiles/farmer-new'))
 
 
