2b334b3db7db20a00401a41ee4a752b0427189c5
Gonzalo Argüelles <garguelles2701@gmail.com>
2020-11-22 10:57:59 +0100

    profile

--- routes/base.routes.js
+++ routes/base.routes.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
+// const Farm = require('../models/farm.model')
+
+// const ensureAuthenticated = (req, res, next) => req.isAuthenticated() ? next() : res.render('auth/login-form', { errorMsg: 'Desautorizado, inicia sesión' })
+// const checkRole = admittedRoles => (req, res, next) => admittedRoles.includes(req.user.role) ? next() : res.render('auth/login-form', { errorMsg: 'Desautorizado, no tienes permisos' })
+
+
 // Endpoints
+
+//router.get('/', checkRole(['FARMER', 'BUYER', 'ADMIN']), (req, res) => res.render('index'{ isFarmer: req.user.role.includes('FARMER'), isAdmin: req.user.role.includes('ADMIN'), isBuyer: req.user.role.includes('BUYER') } ))
+
 router.get('/', (req, res) => res.render('index'))
 
 
