ebd452d6c8009a02106a0af3d840a2b984fe9eb9
Marisa Vitale <marisa.vitale2306@gmail.com>
2020-11-22 19:59:30 +0100

    product details done, buyer profile

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -48,6 +48,34 @@ router.post('/create-farm', (req, res) => {
 })
 
 
+//--------BUYER-----------//
+
+//Create/Edit BUYER FORM (GET)
+router.get('/edit-buyer', (req, res) => {
+
+    const farmId = req.query.id
+
+    Farm
+        .findById(farmId)
+        .then(farmInfo => res.render('profiles/buyer-edit', { farmInfo }))
+        .catch(err => console.log(err))
+
+})
+//Create/Edit BUYER FORM (POST)
+router.post('/edit-buyer', (req, res) => {
+
+    const farmId = req.query.id
+    const { name, surname, username, password, email, profileImg, address } = req.body
+
+
+    Farm
+        .findByIdAndUpdate(farmId, { name, surname, username, password, email, profileImg, address })
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+})
+
+//-------------BUYER-------------//
+
 
 //CREATE Product FORM (GET)
 router.get('/:id/create-product', (req, res) => {
